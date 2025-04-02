dd33203f657733de0531e0987348df6f2f9a2c46
Daniel <daniel.seebacher17@gmail.com>
2025-04-02 11:25:24 +0200

    Solved social media challenge

--- js-basics-and-variables_social-media-post/index.js
+++ js-basics-and-variables_social-media-post/index.js
@@ -9,14 +9,23 @@ console.clear();
 	- a boolean called `isReported` to indicate whether the post has been reported
 */
 
-// --v-- write your code here --v--
-
-// --^-- write your code here --^--
+const postTitle = "Mein Social-Media-Post";
+const text = "Hier ist mein Text";
+let numLikes = 123;
+const createdBy = "Daniel";
+const isReported = false;
 
 /*
 2. Log all variables to the console. Then increase the number of likes by one and log the updated like count. Modify your code from step 1 if necessary.
 */
 
+console.log("Title: " + postTitle);
+console.log("Text: " + text);
+console.log("Number of likes: " + numLikes);
+console.log("Created by: " + createdBy);
+numLikes += 1;
+console.log("New number of likes: " + numLikes);
+console.log("Has this post been reported: " + isReported);
 // --v-- write your code here --v--
 
 // --^-- write your code here --^--
