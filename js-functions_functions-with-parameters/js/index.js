c4e977227ade1b3644466b6525c92e3da653e371
Daniel <daniel.seebacher17@gmail.com>
2025-04-07 15:57:26 +0200

    solved the functions with parameters challenge

--- js-functions_functions-with-parameters/js/index.js
+++ js-functions_functions-with-parameters/js/index.js
@@ -1,49 +1,39 @@
 console.clear();
 
-console.log("-------------");
-console.log("Exercise 1:");
+logSeparator(1);
 /*
 1: The function below is not executed. Can you figure out why? Find the bug and fix it.
 */
 
 // Uncomment the next line of code and figure out why it does not work:
-// logText;
+logText();
 
 function logText() {
   console.log("This text does not appear. Why?");
 }
 
-console.log("-------------");
-console.log("Exercise 2:");
+logSeparator(2);
 /*
 2: Look at the functions below. They are almost identical. Can you find a way to generalize them into a new function with an input parameter? 
    Replace the function calls below with your new function.
 */
 
-function greetBob() {
-  console.log("Welcome Bob, good to see you again!");
-}
+const Bob = "Bob";
+const Alice = "Alice";
+const Mary = "Mary";
 
-function greetAlice() {
-  console.log("Welcome Alice, good to see you again!");
+function greeting(name) {
+  console.log(`Welcome ${name}, good to see you again!`);
 }
-
-function greetMary() {
-  console.log("Welcome Mary, good to see you again!");
-}
-
-greetAlice();
-greetBob();
-greetMary();
+greeting(Bob);
+greeting(Alice);
+greeting(Mary);
 
 // --v-- write your code here --v--
 
-function greet() {}
-
 // --^-- write your code here --^--
 
-console.log("-------------");
-console.log("Exercise 3:");
+logSeparator(3);
 /*
 3: We log a small separation text for each subtask. This is repetitive code, so let’s write a function `logSeparator` that takes the number of the exercise as an input parameter and logs the corresponding separator.
    Then, use this function to replace the existing separator logs in this JavaScript file.
@@ -52,5 +42,8 @@ Note: Separation text refers to the text that is logged in the console before ea
 */
 
 // --v-- write your code here --v--
-
+function logSeparator(number) {
+  console.log("-------------");
+  console.log(`Exercise ${number}:`);
+}
 // --^-- write your code here --^--
