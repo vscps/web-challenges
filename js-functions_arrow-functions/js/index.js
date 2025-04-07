53b153a16a76026e89d4ca87c4356bc39b7e99a0
Daniel <daniel.seebacher17@gmail.com>
2025-04-07 12:07:06 +0200

    Solved the arrow functions challenge

--- js-functions_arrow-functions/js/index.js
+++ js-functions_arrow-functions/js/index.js
@@ -6,7 +6,7 @@ Use implicit returns when possible.
 
 HINT: You can find more information about arrow functions and implicit returns in the handout.
 */
-
+/*
 function getCurrentHour() {
   const now = new Date();
   const currentHour = now.getHours();
@@ -20,19 +20,43 @@ function getCurrentHour() {
     return currentHour - 12 + "pm";
   }
 }
+*/
 
+const getCurrentHour = () => {
+  const now = new Date();
+  const currentHour = now.getHours();
+  if (currentHour === 0) {
+    return "12am";
+  } else if (currentHour === 12) {
+    return "12pm";
+  } else if (currentHour <= 11) {
+    return currentHour + "am";
+  } else {
+    return currentHour - 12 + "pm";
+  }
+};
+/*
 function getVectorLength(x, y, z) {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
 }
+*/
 
+const getVectorLength = (x, y, z) => (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+
+/*
 function cleanInput(string) {
   return string.toLowerCase().trim();
 }
+
+*/
+
+const cleanInput = (string) => string.toLowerCase().trim();
 
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
+/*
 const isOddAndSmall = (number) => {
   if (number > 10) {
     return false;
@@ -45,6 +69,32 @@ const isOddAndSmall = (number) => {
   return true;
 };
 
-const add3 = (a, b, c) => a + b + c;
+*/
 
+function isOddAndSmall(number) {
+  if (number > 10) {
+    return false;
+  }
+
+  if (number % 2 === 0) {
+    return false;
+  }
+
+  return true;
+}
+
+/*
+const add3 = (a, b, c) => a + b + c;
+*/
+
+function add3(a, b, c) {
+  return a + b + c;
+}
+
+/*
 const repeat10 = (string) => string.repeat(10);
+*/
+
+function repeat10(string) {
+  return string.repeat(10);
+}
