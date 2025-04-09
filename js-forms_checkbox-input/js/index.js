660b61e6281476d4593995943d4e1ded417e82e6
Daniel <daniel.seebacher17@gmail.com>
2025-04-09 09:17:15 +0200

    Solved the js forms checkbox challenge

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +13,29 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+tosCheckbox.addEventListener("change", (event) => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+    return;
+  } else {
+    showTosError();
+  }
+});
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  console.log("Box angeklickt: " + tosCheckbox.checked);
+  if (tosCheckbox.checked) {
+    alert("Form submitted");
+    successMessage.removeAttribute("hidden");
+    return;
+  } else {
+    return;
+  }
   // --v-- write your code here --v--
 
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
 });
