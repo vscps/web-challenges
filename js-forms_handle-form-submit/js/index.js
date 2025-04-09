5404b252885c941715e9f0337bf3c7d8da9e3a93
Daniel <daniel.seebacher17@gmail.com>
2025-04-09 08:35:42 +0200

    Fixed a bug in the form submit challenge

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -7,7 +7,10 @@ form.addEventListener("submit", function getFormContent(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
-  console.log(formContent);
-  event.target.reset();
+  const age = Number(data["age"]);
+  const badness = Number(data["badness"]);
+  const ageBadness = age + badness;
+  console.log(ageBadness);
+  //event.target.reset();
   event.target.elements.firstName.focus();
 });
