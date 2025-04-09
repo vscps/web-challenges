8d02a961631002e9f77ef3fe7fac552c99052e25
Daniel <daniel.seebacher17@gmail.com>
2025-04-09 09:57:53 +0200

    Solved the js calculator form challenge

--- js-forms_calculator/js/index.js
+++ js-forms_calculator/js/index.js
@@ -21,9 +21,33 @@ function divide(a, b) {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  const numberA = Number(event.target.elements["numberA"].value);
+  const numberB = Number(event.target.elements["numberB"].value);
+  const operator = event.target.elements["operator"].value;
+  console.log(numberA);
+  console.log(numberB);
+  console.log(operator);
+  let result = 0;
 
-  let result;
+  if (operator == "addition") {
+    result = add(numberA, numberB);
+    console.log(result);
+    return result;
+  } else if (operator == "subtraction") {
+    result = subtract(numberA, numberB);
+    console.log(result);
+    return result;
+  } else if (operator == "multiplication") {
+    result = multiply(numberA, numberB);
+    console.log(result);
+    return result;
+  } else if (operator == "division") {
+    result = divide(numberA, numberB);
+    console.log(result);
+    return result;
+  }
 
+  console.log(result);
   // --v-- write your code here --v--
 
   // --^-- write your code here --^--
