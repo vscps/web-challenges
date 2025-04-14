bf2fe70b9a043f075291b8b9c8811fe7bdbf058f
Daniel <daniel.seebacher17@gmail.com>
2025-04-14 08:43:18 +0200

    Worked on the box color challenge of the input and strings folder and downloaded the loop challenges

--- js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,17 @@
 console.clear();
+
+const box = document.querySelector("[data-js=box]");
+const color = document.querySelector("[data-js=input-color]");
+const radius = document.querySelector("[data-js=input-radius]");
+const rotation = document.querySelector("[data-js=input-rotation");
+
+console.log("color");
+
+color.addEventListener("change", (event) => {
+  const hue = event.target.value;
+  box.style["background-color"] = "rgb(128, 0 , 0";
+  // "hsl(" + hue + "deg 100% 100%);";
+  const test = box.style["background-color"];
+  console.log(event.target.value);
+  console.log(test);
+});
---
+++ js-loops_for-console/index.js
@@ -0,0 +1,4 @@
+console.clear();
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
