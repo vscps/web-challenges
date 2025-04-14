f8bb261e4c094803b96748b6e4c2e2e026dcf999
Daniel <daniel.seebacher17@gmail.com>
2025-04-14 15:51:30 +0200

    Solved the star wars console challenge

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,10 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data.results[2].eye_color);
+}
 
 fetchData();
