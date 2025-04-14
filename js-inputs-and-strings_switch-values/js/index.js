31262eea5351a25605f7c4bb0b114e4917a05cde
Daniel <daniel.seebacher17@gmail.com>
2025-04-14 09:28:01 +0200

    Solved the strings switch values challenge

--- js-inputs-and-strings_switch-values/js/index.js
+++ js-inputs-and-strings_switch-values/js/index.js
@@ -1,8 +1,10 @@
 console.clear();
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
@@ -10,4 +12,10 @@ uppercaseButton.addEventListener("click", () => {
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+});
+
+switchButton.addEventListener("click", () => {
+  const temp = secondInput.value;
+  secondInput.value = firstInput.value;
+  firstInput.value = temp;
 });
