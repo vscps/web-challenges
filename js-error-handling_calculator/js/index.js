046dc8cc210cd107fde0e87a7f1cc5e32aaa584d
Daniel <daniel.seebacher17@gmail.com>
2025-04-15 08:41:08 +0200

    Started work on the error handling challenges

---
+++ js-error-handling_calculator/js/index.js
@@ -0,0 +1,28 @@
+console.clear();
+
+const form = document.querySelector("form");
+const output = form.querySelector("output");
+
+const operations = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+try {}
+    if (b === 0) {
+      throw new Error("Cannot divide by zero!");
+    }
+    return a / b;
+  } catch (error) {
+    return { error: error };
+  }
+  },
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const firstNumber = Number(event.target.firstNumber.value);
+  const secondNumber = Number(event.target.secondNumber.value);
+  const operation = event.target.operation.value;
+  output.innerText = operations[operation](firstNumber, secondNumber);
+});
