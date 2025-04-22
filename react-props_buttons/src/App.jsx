788ee8755ffb986a34c2fa5377e1ce18fab4a2b1
Daniel <daniel.seebacher17@gmail.com>
2025-04-22 16:56:04 +0200

    Completed the react button challenge

--- react-props_buttons/src/App.jsx
+++ react-props_buttons/src/App.jsx
@@ -1,3 +1,14 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Button disabled={false} style="color: red" text="Hallo"></Button>;
+}
+
+function Button({ color, disabled, text }) {
+  function handleClick() {
+    alert("Du hast mich angeklickt!");
+  }
+  return (
+    <button disabled={disabled} style={{ color: color }} onClick={handleClick}>
+      {text}
+    </button>
+  );
 }
