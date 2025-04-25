495eb2dd81f2a57768de0a233d3b0663fc3cf3a7
Daniel <daniel.seebacher17@gmail.com>
2025-04-25 12:43:36 +0200

    Solved the react 2 table reservation challenge

--- react-state-2/react-state-2_table-reservation/src/App.jsx
+++ react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,27 @@
 import Counter from "./components/Counter";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  function addPeople() {
+    setPeople(people + 1);
+    console.log(people);
+  }
+
+  function removePeople() {
+    if (people >= 1) {
+      setPeople(people - 1);
+    }
+    console.log(people);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter onAdd={addPeople} onDelete={removePeople} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
--- react-state-2/react-state-2_table-reservation/src/components/Counter.jsx
+++ react-state-2/react-state-2_table-reservation/src/components/Counter.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 
-export default function Counter() {
-  const [people, setPeople] = useState(0);
-
+export default function Counter({ onAdd, onDelete }) {
   return (
     <>
       <h2>How many people would you like to visit us with?</h2>
@@ -11,7 +9,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="increment people count"
-          onClick={() => setPeople(people + 1)}
+          onClick={onAdd}
         >
           +
         </button>
@@ -19,7 +17,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement people count"
-          onClick={() => setPeople(people - 1)}
+          onClick={onDelete}
         >
           -
         </button>
