585e30cf4220b64a6b8d9535120ddc3a013a718d
Daniel <daniel.seebacher17@gmail.com>
2025-05-12 16:40:40 +0200

    Completed the backend basics server challenges

---
+++ backend-basics/backend-basics_greeting/index.js
@@ -0,0 +1,4 @@
+import { createServer } from "node:http";
+import { server } from "./server.js";
+
+server.listen(8000);
---
+++ backend-basics/backend-basics_routes/server.js
@@ -0,0 +1,16 @@
+import { createServer } from "node:http";
+
+export const server = createServer((request, response) => {
+  if (request.url === "/api/fish/1") {
+    response.statusCode = 200;
+    response.end("Shrimp");
+  } else if (request.url === "/api/fish/2") {
+    response.statusCode = 200;
+    response.end("Anemonefish");
+  } else {
+    response.statusCode = 404;
+    response.end("Not found " + request.url);
+  }
+});
+
+// export const server = …
